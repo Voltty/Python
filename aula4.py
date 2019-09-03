dda0cbb0d377636f744a4d5a8dd3cd5fc28135d0
Voltty <44443488+Voltty@users.noreply.github.com>
2019-09-03 17:02:55 -0300

    Add files via upload
    Quarta aula de python

---
+++ aula4.py
@@ -0,0 +1,53 @@
+# print(type('olá mundo'))
+
+# print(type(2.5))
+
+# print(type(True))
+
+# print(type(1))
+
+# print(2 + 3)
+
+# print(2 - 3)
+
+# print(2 * 3)
+
+# print(2 / 3)
+
+# print(2 // 3)
+
+# print(10 % 6)
+
+#print('abc'+'cde')
+
+#print('abc'*2)
+
+#> maior
+#< menor
+#>= maior ou igual
+#<= menor igual
+#== igual
+#!= diferente
+
+
+
+n1 = int(input('digite um numero: '))
+
+n2 = int(input('digite outro numero: '))
+
+# s = n1 + n2
+# d = n1 / n2
+# sub = n1 - n2
+# m = n1 * n2
+
+# print(f'A soma entre os dois numeros é {s} \na subtração é {sub} \na divisão é {d} \na multiplicação é {m}.')
+
+if n1 > n2:
+	print(f' O número {n1} é maior que o número {n2}')
+elif n1 < n2:
+	print(f'número{n2} é maior que o número {n1}')
+elif n1 == n2:
+	Print(f'O número {n1} e O número {n2} são iguais')
+
+
+
