b590e9a06cafa69c3a79b2a60daaf03e4638f6c2
Voltty <44443488+Voltty@users.noreply.github.com>
2019-09-10 17:05:46 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,83 @@
+print("escolha um numero de 1 a 10 ")
+input("aperte ENTER quando estiver pronto ")
+
+
+r1 = input("seu numero tem a letra 'o' no nome ? [s/n] ")
+
+if r1 == 's':
+	r2 = input('Ele é mutiplo de 2 ? [s/n] ')
+	if r2 == 's':
+		r3 = input("o numero tem  no total 4 letras ? [s/n] ")
+		if r3 == 's':
+			r4 = input('O dobro do seu umeor é maior que 10 ? [s/n] ')
+			if r4 == 's':
+				print('Seu numero é 8. ')
+			else:
+				print('Seu numero é 2. ')
+		else:
+			print('seu numero é 4. ')
+	else:
+		r3 = input("o numero tem  no total 4 letras ? [s/n] ")
+		if r3 == 's':
+			print('Seu numero é 9. ')
+		else: 
+			print('Seu numero é 5. ')
+
+
+else:
+	r2 = input('Ele é mutiplo de 2 ? [s/n] ')
+	if r2 == 's':
+		r3 = input("o numero tem  no total 4 letras ? [s/n] ")
+		if r3 == 's':
+			r4 = input('O dobro do seu numero é maior que 10 ? [s/n] ')
+			if r4 == 's':
+				print('Seu numero é 6. ')
+			else:
+				print('Seu numero é 3. ')
+		else:
+			print('seu numero é 10. ')
+	else:
+		r3 = input("o numero tem  no total 4 letras ? [s/n] ")
+		if r3 == 's':
+			print('Seu numero é 7. ')
+		else:
+			print('Seu numero é 1. ')
+
+
+
+
+p1 = int(Input('Coloque a 1º nota : '))
+p2 = int(Input('Coloque a 2º nota : '))
+p3 = int(Input('Coloque a 3º nota : '))
+
+m = (p1 + p2 + p3) / 3
+
+if media >= 7:
+	print('Você foi aprovado!!')
+elif:
+	
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
