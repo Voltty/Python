cd9344a2a0633af1359332ff1d056550d681a071
Voltty <44443488+Voltty@users.noreply.github.com>
2019-10-15 17:08:09 -0300

    AulaSeiLa

---
+++ AulaSeila.py
@@ -0,0 +1,74 @@
+frase = "vamos aprender strings"
+
+print(frase.lower())
+
+letra = strings(input())
+
+if letra.lower() == 'f':
+	print(sair)
+
+print(frase.upper())
+
+print(frase.lower())
+
+print(frase.title())
+
+sprint(frase.replace(" ", "-"))
+
+print(len(frase))
+
+print(frase[0]) #imprime a primeira letra
+print(frase[1]) #imprime a segunda letra
+print(frase[0:5]) #imprime do 0 ao 4
+print(frase[-1])  #imprime de tras para frente
+print(frase[0:15:3])  #:3 imprime de 3 em 3sair
+print(frase[::-1]) # imprime de traz para frente
+
+if "a" < "b" :
+	print("verdadeiro")
+else:
+	print('falso')
+
+print(ord("A"))
+
+print(chr(1125))
+
+palavra = 'python'
+
+for l in palavra:
+	codigo = ord(L)
+	print(codigo)
+	n_c = codigo + 3
+	n_l = chr(n_c)
+	print(n_l)
+	nova_p =n_p + n_l
+print(n_p)
+
+
+#exercicio
+#1
+
+palavra = str(input('Coloque uma paravra: '))
+for i in palavra:
+	cod = ord(i)
+	cod1 = chr(cod)
+	print(cod1)
+
+
+#2
+
+palavra2 = str(input("Coloque uma palavra: "))
+for i2 in palavra2:
+    code = ord(i2)
+    code1 = chr(code)
+    pa = p + code1
+    print(pa)
+
+
+
+
+
+
+
+
+
