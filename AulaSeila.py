c3606f0eed77e2ce9f70cebbb796890990815568
Voltty <44443488+Voltty@users.noreply.github.com>
2019-10-23 21:00:13 -0300

    Update AulaSeila.py

--- AulaSeila.py
+++ AulaSeila.py
@@ -1,99 +1,88 @@
-frase = "vamos aprender strings"
+# frase = "vamos aprender strings"
 
-print(frase.lower())
+# print(frase.lower())
 
-letra = strings(input())
+# letra = strings(input())
 
-if letra.lower() == 'f':
-	print(sair)
+# if letra.lower() == 'f':
+# 	print(sair)
 
-print(frase.upper())
+# print(frase.upper())
 
-print(frase.lower())
+# print(frase.lower())
 
-print(frase.title())
+# print(frase.title())
 
-sprint(frase.replace(" ", "-"))
+# sprint(frase.replace(" ", "-"))
 
-print(len(frase))
+# print(len(frase))
 
-print(frase[0]) #imprime a primeira letra
-print(frase[1]) #imprime a segunda letra
-print(frase[0:5]) #imprime do 0 ao 4
-print(frase[-1])  #imprime de tras para frente
-print(frase[0:15:3])  #:3 imprime de 3 em 3sair
-print(frase[::-1]) # imprime de traz para frente
+# print(frase[0]) #imprime a primeira letra
+# print(frase[1]) #imprime a segunda letra
+# print(frase[0:5]) #imprime do 0 ao 4
+# print(frase[-1])  #imprime de tras para frente
+# print(frase[0:15:3])  #:3 imprime de 3 em 3sair
+# print(frase[::-1]) # imprime de traz para frente
 
-if "a" < "b" :
-	print("verdadeiro")
-else:
-	print('falso')
+# if "a" < "b" :
+# 	print("verdadeiro")
+# else:
+# 	print('falso')
 
-print(ord("A"))
+# print(ord("A"))
 
-print(chr(1125))
+# print(chr(1125))
 
-palavra = 'python'
+# palavra = 'python'
 
-for l in palavra:
-	codigo = ord(L)
-	print(codigo)
-	n_c = codigo + 3
-	n_l = chr(n_c)
-	print(n_l)
-	nova_p =n_p + n_l
-print(n_p)
+# for l in palavra:
+# 	codigo = ord(L)
+# 	print(codigo)
+# 	n_c = codigo + 3
+# 	n_l = chr(n_c)
+# 	print(n_l)
+# 	nova_p =n_p + n_l
+# print(n_p)
 
-##########exercicio######
-#1
+# ##########exercicio######
+# #1
 
-#palavra = str(input('Coloque uma paravra: '))
-#for i in palavra:
-#	print(i)
-
-
-#2
-#palavra2 = str(input("Coloque outra palavra: "))
-#nova_palavra = ''
-
-#for letra in palavra2:
-#	nova_palavra = nova_palavra + letra
-#
-#	print(nova_palavra)
+# #palavra = str(input('Coloque uma paravra: '))
+# #for i in palavra:
+# #	print(i)
 
 
+# #2
+# #palavra2 = str(input("Coloque outra palavra: "))
+# #nova_palavra = ''
 
-#3
-palavra3 = str(input("Coloque outra palavra: "))
-nova_palavra = ''
-contador = 0
-
-for letra in palavra3:
-	nova_palavra = nova_palavra + letra
-	contador = contador + 1
-	if contador%2 == 0:
-		print(nova_palavra.upper())
-	elif contador%2 != 0:
-		print(nova_palavra)
-
-#4
-palavra4 = str(input("Coloque outra palavra: "))
-
-
-for letra in palavra4:
-	print(letra.replace("a", "4"))
-	print(letra.replace("e", "3"))
-	print(letra.replace("i", "1"))
-	print(letra.replace("t", "7"))
-	print(letra)
+# #for letra in palavra2:
+# 	nova_palavra = nova_palavra + letra
+# 
+# 	print(nova_palavra)
 
 
 
+# #3
+# palavra3 = str(input("Coloque outra palavra: "))
+# nova_palavra = ''
+# contador = 0
+
+# for letra in palavra3:
+# 	nova_palavra = nova_palavra + letra
+# 	contador = contador + 1
+# 	if contador%2 == 0:
+# 		print(nova_palavra.upper())
+# 	elif contador%2 != 0:
+# 		print(nova_palavra)
+
+# #4
+# palavra4 = str(input("Coloque outra palavra: "))
 
 
-
-
-
-
-
-
+# for letra in palavra4:
+# 	print(letra.replace("a", "4"))
+# 	print(letra.replace("e", "3"))
+# 	print(letra.replace("i", "1"))
+# 	print(letra.replace("t", "7"))
+# 	print(letra)
