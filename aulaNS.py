404f33a211eb590694c08f1aea03cbe44b8ebb53
Voltty <44443488+Voltty@users.noreply.github.com>
2019-10-29 17:03:58 -0300

    Exercicio8.py

---
+++ aulaNS.py
@@ -0,0 +1,80 @@
+# ##########exercicio######
+# #1
+
+# #palavra = str(input('Coloque uma paravra: '))
+# #for i in palavra:
+# #	print(i)
+
+
+# #2
+# #palavra2 = str(input("Coloque outra palavra: "))
+# #nova_palavra = ''
+
+# #for letra in palavra2:
+# 	nova_palavra = nova_palavra + letra
+# 
+# 	print(nova_palavra)
+
+
+
+# #3
+# palavra3 = str(input("Coloque outra palavra: "))
+# nova_palavra = ''
+# contador = 0
+
+# for letra in palavra3:
+# 	nova_palavra = nova_palavra + letra
+# 	contador = contador + 1
+# 	if contador%2 == 0:
+# 		print(nova_palavra.upper())
+# 	elif contador%2 != 0:
+# 		print(nova_palavra)
+
+#4
+#palavra5 = str(input("Coloque outra palavra: "))
+#nova_palavra = ''
+
+#for letra1 in palavra5:
+#	nova_palavra = nova_palavra + letra1
+#	nova_palavra
+ 
+#print(nova_palavra)
+
+	
+
+#5
+#palavra4 = str(input("Coloque outra palavra: "))
+#np=""
+#for letra in palavra4:
+#	if letra == "a":
+#		letra = "4"
+#	if letra == "e":
+#		letra = "3"
+#	if letra == "l":
+#		letra = "1"
+#	if letra == "t":
+#		letra = "7"
+#	print(letra)
+
+
+#6
+palavra = input("escolha uma palavra para ver se ela é um palindromo: ")
+print(palavra[::-1])
+
+
+#7
+palavra = input("escolha uma palavra para ver se ela é um palindromo: ")
+arvalap = palavra[::-1]
+
+if palavra == arvalap:
+	print('Essa palavra é um palimdromo!! ')
+else:
+	print('Ela n é um palimdromo!! ')
+
+
+
+
+
+
+
+
