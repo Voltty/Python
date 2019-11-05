0af9ff98beb03228e8b8ec9d427ec5c66a685587
Voltty <44443488+Voltty@users.noreply.github.com>
2019-11-05 16:43:34 -0300

    aula6_2.0.py
    versão mais curta do programa

--- aula6.py
+++ aula6.py
@@ -1,75 +1,47 @@
-print("escolha um numero de 1 a 10 ")
+def pergunta():
+	r1 = input("seu numero tem a letra 'o' no nome ? [s/n] ")
+	return r1
+
+def pergunta2():
+	r2 = input('Ele é mutiplo de 2 ? [s/n] ')
+	return r2
+
+def pergunta3():
+	r3 = input("o numero tem  no total 4 letras ? [s/n] ")
+	return r3
+
+def pergunta4():
+	r4 = input('O dobro do seu numero é maior que 10 ? [s/n] ')
+	return r4
+
+print("escolha um numero de 1 a 10 e guarde para você ")
 input("aperte ENTER quando estiver pronto ")
 
-
-r1 = input("seu numero tem a letra 'o' no nome ? [s/n] ")
-
-if r1 == 's':
-	r2 = input('Ele é mutiplo de 2 ? [s/n] ')
-	if r2 == 's':
-		r3 = input("o numero tem  no total 4 letras ? [s/n] ")
-		if r3 == 's':
-			r4 = input('O dobro do seu umeor é maior que 10 ? [s/n] ')
-			if r4 == 's':
+if pergunta() == 's':
+	if pergunta2() == 's':
+		if pergunta3() == 's':
+			if pergunta4() == 's':
 				print('Seu numero é 8. ')
 			else:
 				print('Seu numero é 2. ')
 		else:
 			print('seu numero é 4. ')
 	else:
-		r3 = input("o numero tem  no total 4 letras ? [s/n] ")
-		if r3 == 's':
+		if pergunta3 == 's':
 			print('Seu numero é 9. ')
 		else: 
 			print('Seu numero é 5. ')
-
-
 else:
-	r2 = input('Ele é mutiplo de 2 ? [s/n] ')
-	if r2 == 's':
-		r3 = input("o numero tem  no total 4 letras ? [s/n] ")
-		if r3 == 's':
-			r4 = input('O dobro do seu numero é maior que 10 ? [s/n] ')
-			if r4 == 's':
+	if pergunta2 == 's':
+		if pergunta3 == 's':
+			if pergunta4 == 's':
 				print('Seu numero é 6. ')
 			else:
 				print('Seu numero é 3. ')
 		else:
-			print('seu numero é 10. ')
+			print('seu numero é 10')		
 	else:
-		r3 = input("o numero tem  no total 4 letras ? [s/n] ")
-		if r3 == 's':
+		if pergunta3 == 's':
 			print('Seu numero é 7. ')
 		else:
 			print('Seu numero é 1. ')
-
-
-
-
-
-	
-
-
-
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
