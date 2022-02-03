ed56e4c9b4ca8b126726ad6a36650bcc7a09a910
Voltty <44443488+Voltty@users.noreply.github.com>
2022-02-03 16:37:47 -0300

    Update note3.py

--- note3.py
+++ note3.py
@@ -1,36 +1,36 @@
 
 
-# for a in range(0,11):
-#     print(a)
-#end
-# for b in range(0,11, -1):
-#     print(b)
-#end
+for a in range(0,11):
+    print(a)
+end
+for b in range(0,11, -1):
+    print(b)
+end
 
-# for c in range(0,11, 2):
-#     print(c)
-#end
+for c in range(0,11, 2):
+    print(c)
+end
 
-# n =  int(input('digite um número: '))
-# for d in range (0,n+1):
-#     print(d)
-# #end
+n =  int(input('digite um número: '))
+for d in range (0,n+1):
+    print(d)
+end
 
-# i = int(input('Inicio: '))
-# f = int(input('Fim: '))
-# p = int(input('passo: '))n
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('passo: '))n
 
-# for e in range(i, f+1, p):
-#     print(e)
-# #end
+for e in range(i, f+1, p):
+    print(e)
+end
 
 
-# q = int(input('Quantas vezes: '))
-# s = 0
-# for n in range(0,q):
-#     n = int(input('Digite um valor: '))
-#     s = s + n
-# print(f'o total foi {s}')
+q = int(input('Quantas vezes: '))
+s = 0
+for n in range(0,q):
+    n = int(input('Digite um valor: '))
+    s = s + n
+print(f'o total foi {s}')
 
 
 
